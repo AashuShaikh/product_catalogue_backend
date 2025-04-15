285792ced04f2c1e930c5c304bd321d2d9fb73e5
AashuShaikh <aashushaikh13dec@gmail.com>
2025-04-16 00:47:47 +0530

    some changes

--- src/main/kotlin/com/aashushaikh/practice_1/controllers/AuthController.kt
+++ src/main/kotlin/com/aashushaikh/practice_1/controllers/AuthController.kt
@@ -2,9 +2,7 @@ package com.aashushaikh.practice_1.controllers
 
 import com.aashushaikh.practice_1.security.AuthService
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
@@ -32,21 +30,17 @@ class AuthController(
 
     @PostMapping("/register")
     fun register(@RequestBody body: AuthRequest) {
-        // Default roles to USER if not provided
         val roles = body.roles ?: setOf("ROLE_USER")
-
-        // If roles are provided, but not by an ADMIN, throw an error (optional logic)
         if (roles != setOf("ROLE_USER") && !hasAdminRole()) {
             throw ResponseStatusException(HttpStatus.UNAUTHORIZED, "Only Admins can register with roles")
         }
-
         authService.register(email = body.email, password = body.password, roles = roles)
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/admin/register")
     fun adminRegister(@RequestBody body: AuthRequest) {
-        val roles = body.roles ?: setOf("ROLE_USER") // Default to USER if no roles provided
+        val roles = body.roles ?: setOf("ROLE_USER")
         authService.register(email = body.email, password = body.password, roles = roles)
     }
 
